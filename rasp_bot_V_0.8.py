981fa7f10104071c4e3fd25be0199c9b5f029310
Joao Vitor <joaovitorsilva57@gmail.com>
2017-04-21 01:13:11 -0300

    Update rasp_bot_V_0.8.py

--- rasp_bot_V_0.8.py
+++ rasp_bot_V_0.8.py
@@ -11,8 +11,10 @@ import urllib2
 
 # função principal do bot estrutura de decisão
 def handle(msg):
+	
 	# configurações da variavel para controlar 
-	# a estrutura de decisao saporra aqui e importante	
+	# a estrutura de decisao saporra aqui e importante
+	# tem tambem a tupla que salva as info do chat 
 	comando = msg['text']
 	content_type, chat_type, chat_id = telepot.glance(msg)
 	m = telepot.namedtuple.Message(**msg)
@@ -22,18 +24,18 @@ def handle(msg):
 	def getinfo(chat_id):
 		if chat_id < 0:
 			print '%s %s %s' % (content_type, m.chat, m.from_)
-			bot.sendMessage(chat_id, 'Grupo legal')
 		elif chat_id == 83074778:
 			bot.sendMessage(chat_id, 'Vai durmir Douglas')
 		elif chat_id == 24774270:
 			bot.sendMessage(chat_id, 'Corre berg')
 		else:
 			print '%s %s' % (content_type, m.chat)
+			print '%s %s ' % (chat_id, chat_username)
 
 	
 	# Aqui e o teclado que aparece para o usuario 
-	# define as palavras que aparecem e as que seram 
-	# enviadas quando forem selecionadas
+	# define as palavras que aparecem e os comanos
+	# que seram enviadas quando forem clicados
 	keyboard=ReplyKeyboardMarkup(
         keyboard=[
             [KeyboardButton(text="Temperatura"), KeyboardButton(text="Memoria")],
@@ -41,9 +43,9 @@ def handle(msg):
             [KeyboardButton(text="Data"),  KeyboardButton(text="IP")],
             [KeyboardButton(text="Processos"), KeyboardButton(text="Ajuda")],
         ])
+	
 	# primeiro IF que inicia o bot quando começa a 
-	# conversar envia apenas messagens de boas vindas, 
-	# desenvolvedor e versao do bot
+	# conversa envia apenas messagens de boas vindas
 	if comando == '/start':
 		bot.sendMessage(chat_id, ' Bem Vindo!!')
 		bot.sendMessage(chat_id, ' Bot desenvolvido por @joao_slv')
@@ -52,6 +54,8 @@ def handle(msg):
 		
 		getinfo(chat_id)
 		
+	# primeira verificaçao verifica se o comando temperatura
+	# foi escolhido se escolhido retorna a temperatura da cpu	
 	elif comando == 'Temperatura':
 		print 'Comando usado ', comando	
 		temp = commands.getoutput("vcgencmd measure_temp | cut -c 6-12")
@@ -60,6 +64,9 @@ def handle(msg):
 		
 		getinfo(chat_id)
 		
+	# segunda verificaçao verifica se o comando memoria
+	# foi escolhido se escolhido retorna a memoria toral
+	# memoria em uso e memoria livre respectivamente
 	elif comando == 'Memoria':
 		print 'Comando usado ', comando
 		memt = commands.getoutput("free -h | grep 'Mem' | cut -c 15-18")
@@ -73,7 +80,9 @@ def handle(msg):
 		bot.sendMessage(chat_id, memf)
 		
 		getinfo(chat_id)
-		
+	
+	# terceira verificaçao verifica se o comando uptime
+	# foi escolhido se escolhido retorna o uptime do sistema 
 	elif comando == 'UpTime':
 		print 'Comando usado ',  comando
 		uptime = commands.getoutput("uptime -p")
@@ -82,6 +91,9 @@ def handle(msg):
 		
 		getinfo(chat_id)
 		
+	# quarta verificaçao verifica se o comando SD 
+	# foi escolhido se escolhido retorna informaçes 
+	# de espaço nas partiçoes de boot e root do sistema
     	elif comando == 'UsoSD':
 		print 'Comando usado ', comando
 		bot.sendMessage(chat_id, 'Estado da partiçao Boot')
@@ -92,7 +104,10 @@ def handle(msg):
 		bot.sendMessage(chat_id, partRoot)
 		
 		getinfo(chat_id)
-		
+	
+	# quinta verificação verifica se o comando data 
+	# foi escolhido se escolhido retorna as 
+	# informaçoes de data e hora atual da raspberry
 	elif comando == 'Data':
 		print 'Comando usado ', comando
 		data = commands.getoutput("date")
@@ -101,20 +116,27 @@ def handle(msg):
 		
 		getinfo(chat_id)
 	
+	# sexta verificação verifica se o comando ip foi executado
+	# se executado verifica o chat_id de quem solicitou se o 
+	# chat_id do solicitante for igual aos listados 
+	# abaixo ele retorna o endereço de ip local e externo
 	elif comando == 'IP':
 		print 'Comando usado ', comando
+		# ip local para min dougras e berg
 		if chat_id == 345318600 or chat_id == 83074778 or chat_id == 24774270:
 			
 			# aqui pega o ip da lan
 			iplan = commands.getoutput("ifconfig wlan0 |  grep inet | cut -c 21-37 | head -1")
 			bot.sendMessage(chat_id, 'Endereço de ip local :')
 			bot.sendMessage(chat_id,str(iplan))
-				
-			# aqui pega o ip externo dessa bagaça 
-			response = urllib2.urlopen('http://bot.whatismyipaddress.com/')
-			ipex = response.read()			
-			bot.sendMessage(chat_id, 'Endereço de ip externo :')
-			bot.sendMessage(chat_id,str(ipex))
+			
+			#ip externo so pra min
+			if chat_id == 345318600:	
+				# aqui pega o ip externo dessa bagaça 
+				response = urllib2.urlopen('http://bot.whatismyipaddress.com/')
+				ipex = response.read()			
+				bot.sendMessage(chat_id, 'Endereço de ip externo :')
+				bot.sendMessage(chat_id,str(ipex))
 			
 			bot.senMessage(chat_id, 'Aqui ta o IP porra!!!!')
 	
@@ -123,6 +145,9 @@ def handle(msg):
   		
 		getinfo(chat_id)
 		
+	# setima verificaçao verifica se o comando processos 
+	# foi escolhido se escolhido ele conta quantos 
+	# processos estao rodando no momento e retorna para o usuario	
     	elif comando == 'Processos':
 		print 'Comando usado ', comando
 		quantProc = commands.getoutput("ps -aux | wc -l")
