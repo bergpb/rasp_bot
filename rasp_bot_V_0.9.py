cdef318912a11c4aad530e8c6872ac3e6c72f909
Joao Vitor <joaovitorsilva57@gmail.com>
2017-04-21 21:54:31 -0300

    Update rasp_bot_V_0.9.py

--- rasp_bot_V_0.9.py
+++ rasp_bot_V_0.9.py
@@ -15,7 +15,7 @@ def handle(msg):
 	# configurações da variavel para controlar 
 	# a estrutura de decisao saporra aqui e importante
 	# tem tambem a tupla que salva as info do chat 
-	comando = msg['text']
+	command = msg['text']
 	content_type, chat_type, chat_id = telepot.glance(msg)
 	m = telepot.namedtuple.Message(**msg)
 	
@@ -33,11 +33,6 @@ def handle(msg):
 			print 'Last Name: %s' % m.chat[5]
 			print 'Enviada por %s' % (m.from_,)
 			print '--------------------------------------'
-		# envia messangens de acordo com o chat id
-		elif chat_id == 83074778:
-			bot.sendMessage(chat_id, 'Seja Bem Vindo Sr. Douglas Zuqueto')
-		elif chat_id == 24774270:
-			bot.sendMessage(chat_id, 'Corre berg')
 		else:
 			print 'Messagem do tipo %s ' % (content_type,)
 			print '--------------------------------------'
@@ -56,12 +51,12 @@ def handle(msg):
             [KeyboardButton(text="Temperatura"), KeyboardButton(text="Memoria")],
             [KeyboardButton(text="UpTime"), KeyboardButton(text="UsoSD")],
             [KeyboardButton(text="Data"),  KeyboardButton(text="IP")],
-            [KeyboardButton(text="Processos"), KeyboardButton(text="Ajuda")],
+            [KeyboardButton(text="Processos"), KeyboardButton(text="Rede")],
         ])
 	
 	# primeiro IF que inicia o bot quando começa a 
 	# conversa envia apenas messagens de boas vindas
-	if comando == '/start':
+	if command == '/start':
 		bot.sendMessage(chat_id, ' Bem Vindo!!')
 		bot.sendMessage(chat_id, ' Bot desenvolvido por @joao_slv')
 		bot.sendMessage(chat_id, ' Iniciando o Bot...')
@@ -71,7 +66,7 @@ def handle(msg):
 		
 	# primeira verificaçao verifica se o comando temperatura
 	# foi escolhido se escolhido retorna a temperatura da cpu	
-	elif comando == 'Temperatura':
+	elif command == 'Temperatura':
 		print '---------------------------'
 		print 'Comando usado ', comando	
 		print '---------------------------'
@@ -84,25 +79,28 @@ def handle(msg):
 	# segunda verificaçao verifica se o comando memoria
 	# foi escolhido se escolhido retorna a memoria toral
 	# memoria em uso e memoria livre respectivamente
-	elif comando == 'Memoria':
+	elif command == 'Memoria':
 		print '---------------------------'
 		print 'Comando usado ', comando
 		print '---------------------------'
-		memt = commands.getoutput("free -h | grep 'Mem' | cut -c 15-18")
+		# memoria total
+		mem_total = commands.getoutput("free -h | grep 'Mem' | cut -c 15-18")
 		bot.sendMessage(chat_id, 'Memoria total: ')
-		bot.sendMessage(chat_id, memt)
-		memu = commands.getoutput("free -h | grep 'Mem' | cut -c 26-29")
+		bot.sendMessage(chat_id, str(mem_total),'M')
+		# memoria em uso
+		mem_used = commands.getoutput("free -h | grep 'Mem' | cut -c 26-29")
 		bot.sendMessage(chat_id, 'Memoria em uso: ')
-		bot.sendMessage(chat_id, memu)
-		memf = commands.getoutput("free -h | grep 'Mem' | cut -c 37-40")
+		bot.sendMessage(chat_id, str(mem_used),'M')
+		# memoria livre
+		mem_free = commands.getoutput("free -h | grep 'Mem' | cut -c 37-40")
 		bot.sendMessage(chat_id, 'Memoria livre: ')
-		bot.sendMessage(chat_id, memf)
+		bot.sendMessage(chat_id, str(mem_free),'M')
 		
 		getinfo(chat_id)
 	
 	# terceira verificaçao verifica se o comando uptime
 	# foi escolhido se escolhido retorna o uptime do sistema 
-	elif comando == 'UpTime':
+	elif command == 'UpTime':
 		print '---------------------------'
 		print 'Comando usado ',  comando
 		print '---------------------------'
@@ -115,26 +113,26 @@ def handle(msg):
 	# quarta verificaçao verifica se o comando SD 
 	# foi escolhido se escolhido retorna informaçes 
 	# de espaço nas partiçoes de boot e root do sistema
-    	elif comando == 'UsoSD':
+    	elif command == 'UsoSD':
 		print '---------------------------'
 		print 'Comando usado ', comando
 		print '---------------------------'
 		bot.sendMessage(chat_id, 'Estado da partiçao root')
 		partRoot = commands.getoutput("df -h | grep '/dev'| head -1")
-		bot.sendMessage(chat_id, partRoot)
+		bot.sendMessage(chat_id, str(partRoot))
 		
 		getinfo(chat_id)
 	
 	# quinta verificação verifica se o comando data 
 	# foi escolhido se escolhido retorna as 
 	# informaçoes de data e hora atual da raspberry
-	elif comando == 'Data':
+	elif command == 'Data':
 		print '---------------------------'
 		print 'Comando usado ', comando
 		print '---------------------------'
-		data = commands.getoutput("date")
+		date = commands.getoutput("date")
  		bot.sendMessage(chat_id,"Data e hora do Sistema: ")
-  		bot.sendMessage(chat_id, str(data))
+  		bot.sendMessage(chat_id, str(date))
 		
 		getinfo(chat_id)
 	
@@ -142,7 +140,7 @@ def handle(msg):
 	# se executado verifica o chat_id de quem solicitou se o 
 	# chat_id do solicitante for igual aos listados 
 	# abaixo ele retorna o endereço de ip local e externo
-	elif comando == 'IP':
+	elif command == 'IP':
 		print '---------------------------'
 		print 'Comando usado ', comando
 		print '---------------------------'
@@ -150,17 +148,17 @@ def handle(msg):
 		if chat_id == 345318600 or chat_id == 83074778 or chat_id == 24774270:
 			
 			# aqui pega o ip da lan   ** eth0 para cabo wlan0 para wifi **
-			iplan = commands.getoutput("ifconfig eth0 |  grep inet | cut -c 21-37 | head -1")
+			ip_lan = commands.getoutput("ifconfig eth0 |  grep inet | cut -c 21-37 | head -1")
 			bot.sendMessage(chat_id, 'Endereço de ip local :')
-			bot.sendMessage(chat_id,str(iplan))
+			bot.sendMessage(chat_id,str(ip_lan))
 			
 			#ip externo so pra min
 			if chat_id == 345318600:	
 				# aqui pega o ip externo dessa bagaça 
 				response = urllib2.urlopen('http://bot.whatismyipaddress.com/')
-				ipex = response.read()			
+				ip_ex = response.read()			
 				bot.sendMessage(chat_id, 'Endereço de ip externo :')
-				bot.sendMessage(chat_id,str(ipex))
+				bot.sendMessage(chat_id,str(ip_ex))
 	
 		else:
 			bot.sendMessage(chat_id, 'Sai daqui voce nao vai ver o endereço de IP!!! ')
@@ -170,7 +168,7 @@ def handle(msg):
 	# setima verificaçao verifica se o comando processos 
 	# foi escolhido se escolhido ele conta quantos 
 	# processos estao rodando no momento e retorna para o usuario	
-    	elif comando == 'Processos':
+    	elif command == 'Processos':
 		print '---------------------------'
 		print 'Comando usado ', comando
 		print '---------------------------'
@@ -180,14 +178,39 @@ def handle(msg):
 		
 		getinfo(chat_id)
 		
-	# IF do menu de ajuda so que ainda nao tem nada de ajudar nessa porra tbm	
-	elif comando == 'Ajuda' or comando == '/help':
+	elif command == 'Rede':
 		print '---------------------------'
 		print 'Comando usado ', comando
 		print '---------------------------'
+		# pegar a quantidade de dados enviados pelo Wireless desde a ultima reinicialização  
+		# trocar o wlan0 pela sua interface wireless
+		rx_wifi = commands.getoutput("cat /sys/class/net/wlan0/statistics/rx_bytes")
+		bot.sendMessage(chat_id,"Quantidade de banda recebida pela rede Wifi: ")
+		bot.sendMessage(chat_id,int(rx_wifi))
+		# pega a quantidade de dados enviados pelo cabo desde a ultima reinicialização
+		# trocar o eth0 pela sua interface de rede cabeada
+		rx_cable = commands.getoutpu("cat /sys/class/net/eth0/statistics/rx_bytes")
+		bot.sendMessage(chat_id,"Quantidade de banda recebida pela rede cabeada: ")
+		bot.sendMessage(chat_id,int(rx_cable))
+		# pega a quantidade de dados recebidas pelo cabo desde a ultima reinicialização
+		# trocar o wlan0 pela sua interface de rede semfio
+		tx_wifi = commands.getoutput("cat /sys/class/net/wlan0/statistics/tx_bytes")
+		bot.sendMessage(chat_id,"Quantidade de banda enviada pela rede Wifi: ")
+		bot.sendMessage(chat_id,int(tx_wifi))
+		# pega a quantidade de dados recebidos pelo cabo desde a ultima reinicialização
+		# trocar o eth0 pela sua interface de rede cabeada
+		tx_cable = commands.getoutput("cat /sys/class/net/eth0/statistics/tx_bytes)
+		bot.sendMessage(chat_id,"Quantidade de banda enviada pela rede cabeada")
+		bot.sendMessage(chat_id,int(tx_cable))
+
+		
+	
+	# IF do menu de ajuda so que ainda nao tem nada de ajudar nessa porra tbm	
+	elif  command == '/help':
+	
 		bot.sendMessage(chat_id, 'Menu de Ajuda')
 		bot.sendMessage(chat_id, 'Aqui era pra ter informações uteis mas nao tem =/')
-		
+					      
 		getinfo(chat_id)
 		
 	# messagem para caso o usuario envie comandos digitados manualmente	
@@ -196,15 +219,11 @@ def handle(msg):
 		bot.sendMessage(chat_id, 'Ainda nao sei ler =(')
 		
 # Aqui fica API do bot gerada pelo botfather
-bot = telepot.Bot('API bot here')
+bot = telepot.Bot(' ')
 bot.message_loop(handle)
 
 print 'Aguardando comandos ...'
 
 while 1:
-time.sleep(5)
-Douglas Zuqueto, [21.04.17 18:26]
-cat /sys/class/net/wlp2s0/statistics/rx_bytes
+	time.sleep(5)
 
-Douglas Zuqueto, [21.04.17 18:26]
-cat /sys/class/net/wlp2s0/statistics/tx_bytes
